b8476490ea60c578cd8e6f2f1d3e9a2e7230772c
Yurii-Yevsiukov <92231@ukr.net>
2024-04-17 19:58:27 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let intervalId;
 
+function getRandomHexColor () {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor () {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+
+  intervalId = setInterval(changeBackgroundColor, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+
+  clearInterval(intervalId);
+});
